29bc38d7ab32ebcb3e47675f5e773aa34ced08af
Oleksnadr Moskalyk <omoskalyk@blackpointcyber.com>
2026-06-13 22:20:24 +0200

    fix error of next.js build error

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <SignIn />
+      <Suspense fallback={null}>
+        <SignIn />
+      </Suspense>
     </div>
   );
 }
